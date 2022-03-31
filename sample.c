4c7744ae732b613f70b54000c4c182e7ee9b0c20
Miyakawa Takuya <omyakawa@gmail.com>
2022-04-01 03:42:11 +0900

    first commit

---
+++ sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //Standard Input Output のヘッダーファイル
+//二行のコマンドで実行する場合
+// gccで実行ファイルの生成
+//./〇〇 で実行
+
+//もしくは次の一行で実行できる
+// gcc -o sample sample.c && ./sample
+
+int main(void)
+{
+    printf("Hello C !!\n");
+    printf("やりますよーーー！！\n");
+    return 0;
+}
